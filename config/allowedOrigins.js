4f38b558728ed73a180209561b9023e792430002
Chris Toews <chris@yourtechguys.info>
2024-11-20 16:58:04 -0500

    adjusted cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,9 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://goodfront-stg-3f37ff5c4bc0.herokuapp.com',
-    'https://goodfront-stg-3f37ff5c4bc0.herokuapp.com'
+    'https://goodfront-stg-3f37ff5c4bc0.herokuapp.com',
+    'http://goodcall-prd-568b0511c747.herokuapp.com',
+    'https://goodcall-prd-568b0511c747.herokuapp.com'
 ];
 
 module.exports = allowedOrigins;
