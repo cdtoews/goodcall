65841d0b9f223b73d4247f0fd27a3487bdf7ca94
Chris Toews <chris@yourtechguys.info>
2024-11-25 16:40:28 -0500

    adding allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,10 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://goodfront-stg-3f37ff5c4bc0.herokuapp.com',
-    'https://goodfront-stg-3f37ff5c4bc0.herokuapp.com',
-    'https//goodfront-665cd6d76a56.herokuapp.com',
-    'https://goodfront-665cd6d76a56.herokuapp.com'
+    'https://callreports-stg-81ce23a9d77e.herokuapp.com'
 ];
 
 module.exports = allowedOrigins;
