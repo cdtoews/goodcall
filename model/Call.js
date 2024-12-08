b580dadaedf961dbad9451c4a7a238db620e8062
Chris Toews <chris@yourtechguys.info>
2024-12-08 18:57:26 -0500

    more virtual

--- model/Call.js
+++ model/Call.js
@@ -15,7 +15,7 @@ const callSchema = new Schema({
     call_date: {
         type: Date,
         required: true,
-        default: Date.now        
+        default: Date.now
     },
     notes: {
         type: String
@@ -32,23 +32,31 @@ const callSchema = new Schema({
     toJSON: { virtuals: true } // <-- include virtuals in `JSON.stringify()`
 });
 
-callSchema.virtual("friendly_date").get(function(){
+callSchema.virtual("friendly_date").get(function () {
     const thisDate = this.call_date;
     const date = new Date(thisDate);
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add 1 to month (0-indexed)
-  const day = date.getDate().toString().padStart(2, '0');
-  const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add 1 to month (0-indexed)
+    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getFullYear();
 
-  return `${month}-${day}-${year}`;
+    return `${month}-${day}-${year}`;
 })
 
-callSchema.virtual("friendly_time").get(function(){
+callSchema.virtual("friendly_time").get(function () {
     const thisDate = this.call_date;
     const date = new Date(thisDate);
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
 
-  return `${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 })
+
+callSchema.virtual("employeeName").get(function () {
+    var str = this.user_id.username;
+    var nameParts = str.split("@");
+    var name = nameParts.length==2 ? nameParts[0] : "";
+    return name;
+})
+
 
 module.exports = mongoose.model('Call', callSchema);
