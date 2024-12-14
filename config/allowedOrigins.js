0efb792f81859253c6190b1d1c579b4ea99a1a6c
Chris Toews <chris@yourtechguys.info>
2024-12-14 16:25:34 -0500

    adjusting allowed origings

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,26 @@
-const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost',
-    'http://toelap4:3000',
-    'http://toelap4',
-    'https://callreports-stg-81ce23a9d77e.herokuapp.com',
-    'https://callreports.up.railway.app'
-];
+require('dotenv').config();
+
+const allowedOrigins = () => {
+    if (process.env.MY_ENV === "PROD") {
+        return ['https://callreports-48ccd3554305.herokuapp.com'];
+    }else if(process.env.MY_ENV === "STG"){
+        return [
+            'https://callreports-stg-81ce23a9d77e.herokuapp.com',
+            'https://callreports.up.railway.app'
+        ];
+    }else if(process.env.MY_ENV === "DEV"){
+        return  [
+            'http://127.0.0.1:5500',
+            'http://localhost:3500',
+            'http://localhost:3000',
+            'http://localhost',
+            'http://toelap4:3000',
+            'http://toelap4'
+        ];
+    }
+
+}
+
+
 
 module.exports = allowedOrigins;
