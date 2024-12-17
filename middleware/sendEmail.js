1527cc06f3921cdf9d1fbd0bb4c1879f69b1b651
Chris Toews <chris@yourtechguys.info>
2024-12-17 15:30:43 -0500

    sendMail changed to Postmark

--- middleware/sendEmail.js
+++ middleware/sendEmail.js
@@ -151,80 +151,48 @@ const sendPwResetEmail = (tempPw, username, duration_text) => {
         }
     });
 
-
-
-
-
-    // const msg = {
-    //     to: username,
-    //     from: fromEmail,
-    //     templateId: 'd-e48ee1b55a624347881e954658e5f22b',
-    //     dynamicTemplateData: {
-    //         email: msgSubject,
-    //         username: username,
-    //         pw_reset_link: pw_reset_link,
-    //         webapp_url: webAppURL,
-    //         duration: duration_text
-    //     },
-    //     hideWarnings: true
-    // };
-
-    // sgMail
-    //     .send(msg)
-    //     .then(() => {
-    //         console.log('PW Reset Email sent')
-    //     })
-    //     .catch((error) => {
-    //         console.error(error)
-    //     })
-    // //email , pw_reset_link 
-
 }
 
 //#region New User
 const sendNewUserEmail = (tempPw, username, duration_text) => {
-    console.log("inside SNUE");
+    //console.log("inside SNUE");
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const fromEmail = process.env.EMAIL_FROM;
     var webAppURL = process.env.WEB_APP_URL;
     const pw_reset_link = `${webAppURL}/pwreset/?user=${username}&pw=${tempPw}`;
-    const msgSubject = "You have been added to Call Reports";
+    
 
-    const msg = {
-        to: username,
-        from: fromEmail,
-        templateId: 'd-06bccbd95b3a4991b1035b9e93527e8b',
-        dynamicTemplateData: {
-            username: username,
-            pw_reset_link: pw_reset_link,
-            webapp_url: webAppURL,
-            duration: duration_text
+    client.sendEmailWithTemplate({
+        "TemplateModel": {
+            "username": username,
+            "pw_reset_link": pw_reset_link,
+            "duration": duration_text,
+            "webapp_url": webAppURL
+
+        },
+        "TemplateId": 38396651,
+        "From": fromEmail,
+        "To": username
+
+
+    }, function (error, success) {
+        if (error) {
+            console.log("ERROR on New User Reset email");
+            console.log(error);
+        } else {
+            console.log("New User Email sent successfully");
+            // console.log(success);
         }
-        ,
-        hideWarnings: true
-    };
+    });
 
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('New User Email sent')
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-    //email , pw_reset_link 
 
 }
-
-
 
 const sendMonthlyEmail = (req, res, next) => {
     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
     console.log(`method=${req.method} path=${req.path} remote_ip=${req.ip}`);
     next();
 }
-
-
 
 module.exports = {
     sendCallEntryEmail,
